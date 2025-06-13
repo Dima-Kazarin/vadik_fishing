e58fee0635d745dc10d08208a59ed561494934b2
Dima <dimakazarin66@gmail.com>
2025-06-13 07:59:55 +0300

    edit 5

--- bot/run.py
+++ bot/run.py
@@ -1,28 +1,25 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.enums import ParseMode
 from aiohttp import web
 from app.handlers import router
 from webhook import setup_webhook_app
 
 TOKEN = '8190188550:AAE_Y4jwVLJqPlFh58BcB7Vg9KR05rJlO3Y'
 
+
 async def main():
-    bot = Bot(token=TOKEN)
+    session = AiohttpSession(proxy='http://proxy.server:3128')
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML, session=session)
     dp = Dispatcher()
     dp.include_router(router)
 
-    # aiohttp сервер
-    webhook_app = setup_webhook_app()
-    runner = web.AppRunner(webhook_app)
-    await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", 8081)  # порт для webhook'а
-    await site.start()
-
-    # Запуск aiogram
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Bot stopped")
+        print('Exit')

70ce4227e80133ef44f14a95b982307cdafbb864
Dima <dimakazarin66@gmail.com>
2025-06-13 08:04:25 +0300

    edit 6

--- bot/run.py
+++ bot/run.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
 from aiohttp import web
@@ -10,8 +11,8 @@ TOKEN = '8190188550:AAE_Y4jwVLJqPlFh58BcB7Vg9KR05rJlO3Y'
 
 
 async def main():
-    session = AiohttpSession(proxy='http://proxy.server:3128')
-    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML, session=session)
+    session = AiohttpSession()
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML), session=session)
     dp = Dispatcher()
     dp.include_router(router)
 
